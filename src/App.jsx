fc0ed0120db1de8fed7324d7fcdd534f0d3b7eac
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-25 14:37:53 +0530

    app-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Counter from "./features/counter/Counter"
 import TodoList from "./features/todolist/Todolist"
 
-
 function App() {
-
   return (
     <div className="border border-2 m-3 p-3 border-dark rounded-3">
       <h1>Redux ToolKit</h1>
@@ -13,4 +11,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
