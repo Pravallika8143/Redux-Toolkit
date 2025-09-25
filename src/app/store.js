e9a95b330a0cbea71bed0db12ce543f997d79d97
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-25 14:35:33 +0530

    store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
-import todolistReducer from '../features/todolist/todolistSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import todolistReducer from '../features/todolist/todolistSlice';
 
 export const store = configureStore({
   reducer: {
